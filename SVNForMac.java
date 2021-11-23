e99a8d3e70f46d65aede0c913eacb117cfad5b44
sunp-github <63430974+sunp-github@users.noreply.github.com>
2021-11-23 15:31:01 +0800

    Add files via upload

---
+++ SVNForMac.java
@@ -0,0 +1,12 @@
+/**
+ * mac m1 安装svn
+ * 
+ * 在终端上安装svn
+ * 		brew install subservion
+ * 
+ * 如果出现brew command not found，需要先安装brew
+ * 		/bin/zsh -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)"
+ * 
+ * 安装好brew后在执行
+ * 		brew install subversion
+ */
